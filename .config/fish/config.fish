8f291494fc94a7149df1f8dc01eec60f9b59f538
Martin Zemlicka <m@zmlka.com>
2014-09-15 15:26:32 +0200

    switch to the right vim on a mac

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,20 @@
 # fish config
 # https://github.com/zmlka
 
+# find out which system we are on
+function mac ; test (uname -s) = "Darwin" ; end
+function linux ; test (uname -s) = "Linux" ; end
 
 # Set the default editor to vim
 set -gx EDITOR vim
 set -gx EDITOR vim
 set -gx GIT_EDITOR vim
 
+
+if mac
+  alias vim /Applications/MacVim.app/Contents/MacOS/Vim
+else
+end
 
 
 
