c55df4985be043d6889724d26b5ae97e4d44b13e
Martin Zemlicka <m@zmlka.com>
2014-09-15 14:57:35 +0200

    fish fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,26 @@
+# fish config
+# https://github.com/zmlka
+
+
 # Set the default editor to vim
 set -gx EDITOR vim
 set -gx EDITOR vim
+set -gx GIT_EDITOR vim
+
+
+
+
+
+# Unicode references
+#
+## → ➜ ‣ ➤ ⬆ ⬅ ⤴ • ■ ● ✜ ❤ ☯
+## ✔ ✖ ✚ ★ ☆ ✩ ✱ ✸ ✿
+## ǁ ǂ ʭ Ξ ⣿ ⡇ ⦀⦂ ⦙⦚ ⧘⧙⧚⧛
+## ⪡ ⪢ ⪦⪧ ⫏⫐ ⊲ ⊳ ⫷ ⫸ ❮❯ ‹›⟨⟩⟪⟫ ⦉⦊⦗⦘ ⎨⎬ ❪❫ 〔 〕
+## ⚑ ⚐ ☐ ✆ ✈ ⚠ ⚡ ♪ ♫ ⦨ ⑆ ⑉ ⧎ ⧑
+## ☼ ☀ ☁ ☂
+## ■ □ ▪▫ ▶ ▷▸▹ ► ▻▼▽▾▿◀◁◂◃◄◅ ▬ ▭▮▯○
+## ▢ ▣ ▦ ◈ ⟐ ⧆ ⧇
+## ⊕ ⊝ ⊜ ⊗ ⊙ ⊚ ⦼ ⧀ ⦿ ⧁ ⨂ ⨁ ⨀ ◉ ◌ ◍
+## 〓 █████ ▇▇▇▆▅▅▄▃▂▁▀▗▖▘▙▚▛▜ ◹ ◺ ◸ ◿
+## ⌘ ⌥ ⌦ ⌫ ↵ ↩ ⇧l
