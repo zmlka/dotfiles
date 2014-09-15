3874b1719355fcf008df6f713b3faf3ef0e77486
Martin Zemlicka <m@zmlka.com>
2014-09-15 22:06:25 +0200

    prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,32 +1,23 @@
-function fish_prompt --description 'Write out the prompt'
-	set -l last_status $status
+function fish_prompt
+	set_color yellow
+   printf '%s' (whoami)
+   set_color normal
+   printf ' at '
 
-  # User
-  set_color $fish_color_user
-  echo -n (whoami)
-  set_color normal
+   set_color magenta
+   printf '%s' (hostname|cut -d . -f 1)
+   set_color normal
+   printf ' in '
 
-  echo -n '@'
+   set_color $fish_color_cwd
+   printf '%s' (prompt_pwd)
+   set_color normal
 
-  # Host
-  set_color $fish_color_host
-  echo -n (hostname -s)
-  set_color normal
-
-  echo -n ':'
-
-  # PWD
-  set_color $fish_color_cwd
-  echo -n (prompt_pwd)
-  set_color normal
-
-  __terlar_git_prompt
-  echo
-
-  if not test $last_status -eq 0
-    set_color $fish_color_error
-  end
-
-  echo -n '➤ '
-  set_color normal
+   # Line 2
+   echo
+   if test $VIRTUAL_ENV
+       printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
+   end
+   printf '↪ '
+   set_color normal
 end
