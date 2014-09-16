cd8873ff64f7f306ccf1c0ca8ec9a992cb15467f
Martin Zemlicka <m@zmlka.com>
2014-09-16 14:55:03 +0200

    fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,32 +1,54 @@
-function fish_prompt --description 'Write out the prompt'
-	set -l last_status $status
+# name: clearance
+# ---------------
+# Based on idan. Display the following bits on the left:
+# - Virtualenv name (if applicable, see https://github.com/adambrenecki/virtualfish)
+# - Current directory name
+# - Git branch and dirty state (if inside a git repo)
 
-  # User
-  set_color $fish_color_user
-  echo -n (whoami)
-  set_color normal
+function _git_branch_name
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
 
-  echo -n '@'
+function _git_is_dirty
+  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+end
 
-  # Host
-  set_color $fish_color_host
-  echo -n (hostname -s)
-  set_color normal
+function fish_prompt
+  set -l cyan (set_color cyan)
+  set -l yellow (set_color yellow)
+  set -l red (set_color red)
+  set -l blue (set_color blue)
+  set -l green (set_color green)
+  set -l normal (set_color normal)
 
-  echo -n ':'
+  set -l cwd $blue(pwd | sed "s:^$HOME:~:")
 
-  # PWD
-  set_color $fish_color_cwd
-  echo -n (prompt_pwd)
-  set_color normal
+  # Output the prompt, left to right
 
-  __terlar_git_prompt
-  echo
+  # Add a newline before new prompts
+  echo -e ''
 
-  if not test $last_status -eq 0
-    set_color $fish_color_error
+  # Display [venvname] if in a virtualenv
+  if set -q VIRTUAL_ENV
+      echo -n -s (set_color -b cyan black) '[' (basename "$VIRTUAL_ENV") ']' $normal ' '
   end
 
-  echo -n '➤ '
-  set_color normal
+  # Print pwd or full path
+  echo -n -s $cwd $normal
+
+  # Show git branch and status
+  if [ (_git_branch_name) ]
+    set -l git_branch (_git_branch_name)
+
+    if [ (_git_is_dirty) ]
+      set git_info '(' $yellow $git_branch "±" $normal ')'
+    else
+      set git_info '(' $green $git_branch $normal ')'
+    end
+    echo -n -s ' · ' $git_info $normal
+  end
+
+  # Terminate with a nice prompt char
+  echo -e ''
+  echo -e -n -s '⟩ ' $normal
 end
